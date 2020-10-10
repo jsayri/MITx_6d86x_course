da3cbed34709aadc09633f8b1f94e6bad26447e1
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2020-10-10 16:47:02 +0200

    P2. Principal Component Analysis, project_onto_PC functionality was added for the PCA analysis, main function updated for PCA executions.

--- units/unit_2/project_2/main.py
+++ units/unit_2/project_2/main.py
@@ -123,8 +123,6 @@ def run_softmax_on_MNIST(temp_parameter=1, error_mod3 = False):
 # 6. Changing Labels
 #######################################################################
 
-
-
 def run_softmax_on_MNIST_mod3(temp_parameter=1):
     """
     Trains Softmax regression on digit (mod 3) classifications.
