8e2070ad7fd39e817022848a1d3b8c35228af7c7
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2020-11-06 01:19:49 +0100

    P3, section 8, implementation of  mstep, acepted by the grader

--- units/unit_4/project_4/em.py
+++ units/unit_4/project_4/em.py
@@ -18,7 +18,32 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         float: log-likelihood of the assignment
 
     """
-    raise NotImplementedError
+    # variable initialization
+    n, d = X.shape
+    k, _ = mixture.mu.shape
+    post = np.zeros((n, k))  # soft assignation (posterior prob : p(j|i))
+    log_lh = 0.  # init log-likelihood
+
+    # gaussian model: exp(-1/(2 * var) * ||x-u||^2) / (2 * pi * var)^(d/2)
+    N = lambda xi, mu, var, d: np.divide(np.exp(np.divide(((xi-mu)**2).sum(axis=1), -2*var)), (2*np.pi*var)**(d/2))
+    # log prior expression: log(p_i) + log(N(xi, mu, var, d))
+    fui_fun = lambda xi, p, mu, var, d: np.log(p + 1e-16) + np.log(N(xi, mu, var, d))
+
+    # iteration by cluster, matrix calculation
+    for ii, xi in enumerate(X):
+        # select non-zero elements
+        cu_pos = xi > 0
+        xcu = xi[cu_pos] # build non zero vector from xi
+        # calculate log expression for probability at each cluster
+        f_ui = fui_fun(xcu, mixture.p, mixture.mu[:, cu_pos], mixture.var, xcu.size)
+        log_post = f_ui - logsumexp(f_ui)
+        # calculate posterior prob for xi
+        post[ii, :] = np.exp(log_post)
+
+        # log-likelihood as the sum of each i and j
+        log_lh += post[ii, :] @ (f_ui - log_post)
+
+    return post, log_lh
 
 
 
@@ -37,7 +62,32 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    # variable initialization
+    n, d = X.shape
+    _, k = post.shape
+
+    mu_old = mixture.mu
+    mu_new = np.zeros((k, d))
+    var_new = np.zeros(k)
+    dcu = X > 0  # indicator matrix
+
+    # get new parameters
+    p_new = np.sum(post, axis=0) / n
+    for jj in range(k):
+        # update average 'mu'
+        sum_post = (np.tile(post[:, jj], (d, 1)).T * dcu).sum(axis=0)
+        mu_new[jj] = post[:, jj] @ X / sum_post
+        if any(sum_post < 1): # avoid erratic results if few points
+            mu_new[jj, sum_post < 1] = mu_old[jj, sum_post < 1]
+        # update variance 'var'
+        dist_x_mu = ((X - mu_new[jj]*dcu)**2).sum(axis=1)
+        var_new[jj] = post[:, jj] @ dist_x_mu / ((dcu.sum(axis=1) * post[:, jj]).sum())
+
+    # corrections for numerical stability
+    var_new[var_new < min_variance] = min_variance
+
+    return GaussianMixture(mu_new, var_new, p_new)
+
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
--- units/unit_4/project_4/test.py
+++ units/unit_4/project_4/test.py
@@ -9,4 +9,23 @@ K = 4
 n, d = X.shape
 seed = 0
 
-# TODO: Your code here
+
+# implementation of E-step
+gmm_init, _ = common.init(X, K, seed) # gaussian initialization
+post, log_lh = em.estep(X, gmm_init) # run e-step
+# expected result from test_solution.txt: LL=-152.16319226209848
+print('Log-likelihood for E-step: {}'.format(log_lh))
+
+
+# implementation of M-step
+g_mix_new = em.mstep(X, post, gmm_init)
+print('Updated gaussian mixture\n', g_mix_new)
+
+
+# implementation of full run
+
+
+
+# check m-step execution
+gmm_init, post_init = common.init(X, K, seed)  # gaussian initialization
+gmm_final, post_end, log_lh, ll_vec = em.run(X, gmm_init, post_init)
