090ab760cb3a02d1c1dab7b9e7e493049df24df1
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2020-11-06 11:06:46 +0100

    P4, section 8. Using the mixture model for collaborative filtering. Solution for Reporting log likelihood values on Netflix data, test.py code was updated with a routine to run em algorithm on netflix incomplete data for k=1 and 12, and seeds from 0 to 4. Optimal solutions were store at a text file (em_results_netflix_incomplete.txt), E step was updated with a log version for N function. M Step was updated with a condition check before updated mu values.

--- units/unit_4/project_4/em.py
+++ units/unit_4/project_4/em.py
@@ -26,8 +26,11 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
 
     # gaussian model: exp(-1/(2 * var) * ||x-u||^2) / (2 * pi * var)^(d/2)
     N = lambda xi, mu, var, d: np.divide(np.exp(np.divide(((xi-mu)**2).sum(axis=1), -2*var)), (2*np.pi*var)**(d/2))
+    logN = lambda xi, mu, var, d: -d/2*np.log((2 * np.pi * var)) + np.divide(((xi - mu)**2).sum(axis=1), -2 * var)
     # log prior expression: log(p_i) + log(N(xi, mu, var, d))
-    fui_fun = lambda xi, p, mu, var, d: np.log(p + 1e-16) + np.log(N(xi, mu, var, d))
+    # fui_fun = lambda xi, p, mu, var, d: np.log(p + 1e-16) + np.log(N(xi, mu, var, d) + 1e-16)
+    fui_fun = lambda xi, p, mu, var, d: np.log(p + 1e-16) + logN(xi, mu, var, d)
+
 
     # iteration by cluster, matrix calculation
     for ii, xi in enumerate(X):
@@ -76,9 +79,14 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     for jj in range(k):
         # update average 'mu'
         sum_post = (np.tile(post[:, jj], (d, 1)).T * dcu).sum(axis=0)
-        mu_new[jj] = post[:, jj] @ X / sum_post
         if any(sum_post < 1): # avoid erratic results if few points
             mu_new[jj, sum_post < 1] = mu_old[jj, sum_post < 1]
+            ok_idx = sum_post >= 1
+            if ok_idx.sum() >= 1:
+                mu_new[jj, ok_idx] = post[:, jj] @ X[:, ok_idx] / sum_post[ok_idx]
+
+        else:
+            mu_new[jj] = post[:, jj] @ X / sum_post
         # update variance 'var'
         dist_x_mu = ((X - mu_new[jj]*dcu)**2).sum(axis=1)
         var_new[jj] = post[:, jj] @ dist_x_mu / ((dcu.sum(axis=1) * post[:, jj]).sum())
@@ -109,7 +117,7 @@ def run(X: np.ndarray, mixture: GaussianMixture,
     min_th = 10 ** -6
     ll, ll_old = 1, -np.inf
     ll_error = 1
-
+    ll_vec = []
     while ll_error > min_th * abs(ll):
         # E-step
         post, ll = estep(X, mixture)
@@ -120,8 +128,9 @@ def run(X: np.ndarray, mixture: GaussianMixture,
         # next iteration update
         ll_error = ll - ll_old
         ll_old = ll
+        ll_vec.append(ll)
 
-    return mixture, post, ll
+    return mixture, post, ll, ll_vec
 
 
 def fill_matrix(X: np.ndarray, mixture: GaussianMixture) -> np.ndarray:
--- units/unit_4/project_4/test.py
+++ units/unit_4/project_4/test.py
@@ -2,28 +2,85 @@ import numpy as np
 import em
 import common
 
-X = np.loadtxt("test_incomplete.txt")
-X_gold = np.loadtxt("test_complete.txt")
+# EM routine for multiples cluster and seeds
+def em_for_k_and_seeds(X, Ks, Seeds, verbose=True):
+    '''
+    Execution of EM algorithm for a matrix incomplete data. It will
+    check a set of clusters (Ks) for multiples seeds
+    Inputs
+    X :         np.ndarray, matrix of incomplete data
+    Ks :        list, values of clusters to test
+    Seeds :     list, values of random seeds
+    verbose :   boolean, display results, default 'True'
 
-K = 4
-n, d = X.shape
-seed = 0
+    Outputs
+    opt_seed :  np.ndarray, value of optimal seed for each cluster
+    '''
+
+    # init test values
+    log_lh_m = np.zeros((Ks.shape[0], Seeds.shape[0]))
+
+    # Iterative execution on clusters & seeds
+    for ii, k in enumerate(Ks):
+        for jj, seed in enumerate(Seeds):
+            # initialization for em algorithm
+            mixture, post = common.init(X, k, seed)
+
+            # execute EM algorithm with gaussian mixture
+            mixture, post, cost, _ = em.run(X, mixture, post)
+
+            # store each cost at each k and seed
+            log_lh_m[ii, jj] = cost
+            if verbose:
+                print('k={}, seed={}, cost={}'.format(k, seed, round(cost, 5)))
+
+            # display cost function
+            # common.cost_plot(cost_vec)
+
+    # get minimum for each cluster
+    print('Optimal cost at each cluster for multiples seeds.')
+    op_seed = np.zeros(len(Ks))
+    for ii, k in enumerate(Ks):
+        op_seed[ii] = np.argmax(log_lh_m[ii, :])
+        print('k={}, max_cost={}, seed={}'.format(k, np.max(log_lh_m[ii, :]).round(6), op_seed[ii]))
+
+    return op_seed
 
 
-# implementation of E-step
-# gmm_init, _ = common.init(X, K, seed) # gaussian initialization
-# post, log_lh = em.estep(X, gmm_init) # run e-step
-# expected result from test_solution.txt: LL=-152.16319226209848
-# print('Log-likelihood for E-step: {}'.format(log_lh))
+
+if __name__ == "__main__":
+    X = np.loadtxt("test_incomplete.txt")
+    X_gold = np.loadtxt("test_complete.txt")
+
+    K = 4
+    n, d = X.shape
+    seed = 0
 
 
-# implementation of M-step
-# g_mix_new = em.mstep(X, post, gmm_init)
-# print('Updated gaussian mixture\n', g_mix_new)
+    # implementation of E-step
+    # gmm_init, _ = common.init(X, K, seed) # gaussian initialization
+    # post, log_lh = em.estep(X, gmm_init) # run e-step
+    # expected result from test_solution.txt: LL=-152.16319226209848
+    # print('Log-likelihood for E-step: {}'.format(log_lh))
 
 
-# implementation of full run
-gmm_init, post = common.init(X, K, seed) # gaussian initialization
-gmm_end, post, cost = em.run(X, gmm_init, post)
-print('result log-likelihood: \n', cost)
+    # implementation of M-step
+    # g_mix_new = em.mstep(X, post, gmm_init)
+    # print('Updated gaussian mixture\n', g_mix_new)
+
+
+    # implementation of full run
+    # gmm_init, post = common.init(X, K, seed) # gaussian initialization
+    # gmm_end, post, cost = em.run(X, gmm_init, post)
+    # print('result log-likelihood: \n', cost)
+
+
+    # test algorithm with Netflix incomplete data
+    X_netflix = np.loadtxt("netflix_incomplete.txt")
+
+    # scenarios' definition
+    Ks = np.array([1, 12]) # clusters
+    Seeds = np.array([0, 1, 2, 3, 4]) # random seeds
+    # em algorithm execution
+    opseeds = em_for_k_and_seeds(X_netflix, Ks, Seeds)
 
