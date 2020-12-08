5954faceab073a1fafe8056b049b59b096823020
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2020-12-08 13:36:39 +0100

    solution update: a transition matrix and a reward vector are define. Calculations are made by matrix multiplication now. Step by step execution available in a previous version

--- units/unit_5/L17_s7_value_iteration.py
+++ units/unit_5/L17_s7_value_iteration.py
@@ -6,6 +6,7 @@ import numpy as np
 # states
 s = ['s'+str(num) for num in range(0,5)]
 a = ['ml', 'mr', 's'] # ml,mr: move left/right, s: stay
+ad = {'ml': 0, 'mr': 1, 's': 2} # action dictionary
 
 # Transition matrix
 T = np.zeros((len(s), len(a), len(s))) # states, actions, future states prob
@@ -13,92 +14,69 @@ T = np.zeros((len(s), len(a), len(s))) # states, actions, future states prob
 # IMPORTANT!!!
 # Reward Note: The reward function is defined to be R(s,a,s′)=R(s), R(s=5)=1 and R(s)=0 otherwise
 
-# If the agent chooses to move (either left or right) at any of the inner grid locations, such an action
-# is successful with probability 1/3 and with probability 2/3 it fails to move
-# move to the left
-T[1:, 0, 0:4] = 1/3
-T[1:, 0, 1:4] = 2/3
+#  If the agent chooses to stay at the location, such an action is successful with probability 1/2
+T[0, 2, 0] = .5
+T[1, 2, 1] = .5
+T[2, 2, 2] = .5
+T[3, 2, 3] = .5
+T[4, 2, 4] = .5
 
-# move to the right
-T[0:4, 1, 1:] = 1/3
-T[0:4, 1, 0:4] = 2/3
+# if the agent is at the leftmost or rightmost grid location it ends up at its neighboring grid
+# location with probability 1/2
+T[0, 2, 1] = .5
+T[4, 2, 3] = .5
+
+# if the agent is at any of the inner grid locations it has a probability 1/4 each of ending up
+# at either of the neighboring locations.
+T[1, 2, 0], T[1, 2, 2] = .25, .25
+T[2, 2, 1], T[2, 2, 3] = .25, .25
+T[3, 2, 2], T[3, 2, 4] = .25, .25
+
+# If the agent chooses to move (either left or right) at any of the inner grid locations, such an
+# action is successful with probability 1/3 and with probability 2/3 it fails to move, and
+# move left, fails to move left
+T[1, 0, 0], T[1, 0, 1] = 1/3, 2/3
+T[2, 0, 1], T[2, 0, 2] = 1/3, 2/3
+T[3, 0, 2], T[3, 0, 3] = 1/3, 2/3
+T[4, 0, 3], T[4, 0, 4] = 1/3, 2/3
+
+# move right, fails to move right
+T[0, 1, 1], T[0, 1, 0] = 1/3, 2/3
+T[1, 1, 2], T[1, 1, 1] = 1/3, 2/3
+T[2, 1, 3], T[2, 1, 2] = 1/3, 2/3
+T[3, 1, 4], T[3, 1, 3] = 1/3, 2/3
+
+# if the agent chooses to move left at the leftmost grid location, then the action ends up exactly
+# the same as choosing to stay, i.e., staying at the leftmost grid location with probability 1/2,
+# and ends up at its neighboring grid location with probability 1/2
+T[0, 0, 0], T[0, 0, 1] = .5, .5 # move left at the leftmost grid location
+T[4, 1, 4], T[4, 1, 3] = .5, .5 # move right at the rightmost grid location
 
 
-# if the agent chooses to move left at the leftmost grid location, then the action ends up exactly the
-# same as choosing to stay, i.e., staying at the leftmost grid location with probability 1/2, and ends
-# up at its neighboring grid location with probability 1/2
-T[0, 0, 0] = .5
-T[0, 0, 1] = .5
+# The reward function is defined to be R(s,a,s′)=R(s), R(s=5)=1 and R(s)=0 otherwise.
+# Reward table
+R = np.zeros(len(s)) # reward success, rows: states, columns: all actions (R(s))
+#Rs = np.zeros((len(s), len(a))) # reward success, rows: states, columns: actions (ml, mr, s)
+#Rf = np.zeros((len(s), len(a))) # reward fails
+R[4] = 1
 
-# if the agent chooses to move right at the rightmost grid location, then the action ends up exactly the
-# same as choosing to stay, i.e., staying at the rightmost grid location with probability 1/2, and ends
-# up at its neighboring grid location with probability 1/2.
-T[4, 1, 4] = .5
-T[4, 1, 3] = .5
-
+# gamma value
 g = .5
 
 # Value iteration algorithm
-n_iter = 101
-
-V_init = np.zeros(len(s))
+n_iter = 100
 
 V = np.zeros((n_iter, len(s))) # iteration matrix
-V[0, :] = V_init # initialization
 for k in range(1, n_iter):
 
-    # previous iteration
+    # get previous iteration
     Vj = V[k-1, :]
 
-    # at s=0
-    # stay: T(0,s,0)*(R(0,s,0)+g*Vopt(0)) + T(0,s,1)*(R(0,s,1)+g*Vopt(1))
-    V_s = .5 * (0 + g * Vj[0]) + .5 * (0 + g * Vj[1])
-    # left: T(0,l,0)*(R(0,l,0)+g*Vopt(0)) + T(0,l,1)*(R(0,l,1)+g*Vopt(1))
-    V_l = .5 * (0 + g * Vj[0]) + .5 * (0 + g * Vj[1])
-    # right: T(0,r,0)*(R(0,r,0)+g*Vopt(0)) + T(0,r,1)*(R(0,l,1)+g*Vopt(1))
-    V_r = 2/3 * (0 + g * Vj[0]) + 1/3 * (0 + g * Vj[1])
-
-    V[k, 0] = np.max([V_s, V_l, V_r])
-
-    # at s=1
-    # stay: T(1,s,1)*(R(1,s,1) + g*Vopt(1)) + T(1,s,0)*(R(1,s,0) + g*Vopt(0)) + T(1,s,2)*(R(1,s,2) + g*Vopt(2))
-    V_s = .5 * (0 + g * Vj[1]) + .25 * (0 + g * Vj[0]) + .25 * (0 + g * Vj[2])
-    # left: T(1,l,1)*(R(1,l,1) + g*Vopt(1)) + T(1,l,0)*(R(1,l,0) + g*Vopt(0))
-    V_l = 2 / 3 * (0 + g * Vj[1]) + 1 / 3 * (0 + g * Vj[0])
-    # right: T(1,r,1)*(R(1,r,1) + g*Vopt(1)) + T(1,r,2)*(R(1,r,2) + g*Vopt(2))
-    V_r = 2 / 3 * (0 + g * Vj[1]) + 1 / 3 * (0 + g * Vj[2])
-
-    V[k, 1] = np.max([V_s, V_l, V_r])
-
-    # at s=2
-    # stay: T(2,s,2)*(R(2,s,2) + g*Vopt(2)) + T(2,s,1)*(R(2,s,1) + g*Vopt(1)) + T(2,s,3)*(R(2,s,3) + g*Vopt(3))
-    V_s = .5 * (0 + g * Vj[2]) + .25 * (0 + g * Vj[1]) + .25 * (0 + g * Vj[3])
-    # left: T(2,l,2)*(R(2,l,2) + g*Vopt(2)) + T(2,l,1)*(R(2,l,1) + g*Vopt(1))
-    V_l = 2 / 3 * (0 + g * Vj[2]) + 1 / 3 * (0 + g * Vj[1])
-    # right: T(2,r,2)*(R(2,r,2) + g*Vopt(2)) + T(2,r,3)*(R(2,r,3) + g*Vopt(3))
-    V_r = 2 / 3 * (0 + g * Vj[2]) + 1 / 3 * (0 + g * Vj[3])
-
-    V[k, 2] = np.max([V_s, V_l, V_r])
-
-    # at s=3
-    # stay: T(3,s,3)*(R(3,s,3) + g*Vopt(3)) + T(3,s,2)*(R(3,s,2) + g*Vopt(2)) + T(3,s,4)*(R(3,s,4) + g*Vopt(4))
-    V_s = .5 * (0 + g * Vj[3]) + .25 * (0 + g * Vj[2]) + .25 * (0 + g * Vj[4])
-    # left: T(3,l,3)*(R(3,l,3) + g*Vopt(3)) + T(3,l,2)*(R(3,l,2) + g*Vopt(2))
-    V_l = 2 / 3 * (0 + g * Vj[3]) + 1 / 3 * (0 + g * Vj[2])
-    # right: T(3,r,3)*(R(3,r,3) + g*Vopt(3)) + T(3,r,4)*(R(3,r,4) + g*Vopt(4))
-    V_r = 2 / 3 * (0 + g * Vj[3]) + 1 / 3 * (0 + g * Vj[4])
-
-    V[k, 3] = np.max([V_s, V_l, V_r])
-
-    # at s=4
-    # stay: T(4,s,4)*(R(4,s,4) + g*Vopt(4)) + T(4,s,3)*(R(4,s,3) + g*Vopt(3))
-    V_s = .5 * (1 + g * Vj[4]) + .5 * (1 + g * Vj[3])
-    # left: T(4,l,4)*(R(4,l,4) + g*Vopt(4)) + T(4,l,3)*(R(4,l,3) + g*Vopt(3))
-    V_l = 2 / 3 * (1 + g * Vj[4]) + 1 / 3 * (1 + g * Vj[3])
-    # right: T(4,r,4)*(R(4,r,4) + g*Vopt(4)) + T(4,r,3)*(R(4,r,3) + g*Vopt(3))
-    V_r = .5 * (1 + g * Vj[4]) + .5 * (1 + g * Vj[3])
-
-    V[k, 4] = np.max([V_s, V_l, V_r])
+    # at each state-transition Vk*[s]=max_a(sum_s'(T(s,a,s') * (R(s) + g * Vj(s)) ) )
+    for ii, ss in enumerate(s):
+        # Vdiscount = R + g * Vj # consider R(s,a,s') = R(s') (not in this problem)
+        Vdiscount = R[ii] + g * Vj # consider R(s,a,s') = R(s)
+        V[k, ii] = np.max(T[ii] @ Vdiscount.T)
 
 print('end execution')
 
