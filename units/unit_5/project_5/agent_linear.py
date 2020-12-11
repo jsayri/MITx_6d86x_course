0add24a2ad615306efbe317a378c5477e494fd04
J SAYRITUPAC <6142180+jsayri@users.noreply.github.com>
2020-12-11 05:05:15 +0100

    linear agent execution, convergence to 0.418503

--- units/unit_5/project_5/agent_linear.py
+++ units/unit_5/project_5/agent_linear.py
@@ -11,11 +11,11 @@ DEBUG = False
 GAMMA = 0.5  # discounted factor
 TRAINING_EP = 0.5  # epsilon-greedy parameter for training
 TESTING_EP = 0.05  # epsilon-greedy parameter for testing
-NUM_RUNS = 10
+NUM_RUNS = 5
 NUM_EPOCHS = 600
 NUM_EPIS_TRAIN = 25  # number of episodes for training at each epoch
 NUM_EPIS_TEST = 50  # number of episodes for testing
-ALPHA = 0.001  # learning rate for training
+ALPHA = 0.01  # learning rate for training
 
 ACTIONS = framework.get_actions()
 OBJECTS = framework.get_objects()
@@ -50,7 +50,7 @@ def epsilon_greedy(state_vector, theta, epsilon):
         action_index, object_index = np.random.randint(NUM_ACTIONS), np.random.randint(NUM_OBJECTS)
     else:
         # Exploitation strategy
-        action_index, object_index = np.unravel_index((theta @ state_vector).argmax(), (NUM_ACTIONS, NUM_OBJECTS))
+        action_index, object_index = index2tuple((theta @ state_vector).argmax())
 
     return (action_index, object_index)
 # pragma: coderesponse end
@@ -68,7 +68,7 @@ def linear_q_learning(theta, current_state_vector, action_index, object_index,
         object_index (int): index of the current object
         reward (float): the immediate reward the agent recieves from playing current command
         next_state_vector (np.ndarray): vector representation of next state
-        terminal (bool): True if this epsiode is over
+        terminal (bool): True if this episode is over
 
     Returns:
         None
